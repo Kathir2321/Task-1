90484b25eeed7c8cc4fe7d801db726b710ff2ab6
Kathir2321 <kathir.paneerselvam@sparkouttech.com>
2023-10-06 11:19:57 +0530

    initial commit

---
+++ Unique value/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$a=array('Batman', 'Superman', 'Aquaman', 'Batman', 'Spiderman', 'Superman');
+print_r(array_unique($a));
+?>
+</body>
+</html>
---
+++ uppercase/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php     
+$inputString = 'im working in sparkouttech as a trainee'; 
+$uppercaseString = strtoupper($inputString);
+$capitalizedString = ucfirst($inputString);
+
+echo "Uppercase: $uppercaseString\n","<br>";
+echo "Capitalized: $capitalizedString\n";
+?>
+</body>
+</html>
